64fd682a5be0ff232ee837eb9f2e0931b02dc9d2
vergaloy <42758185+vergaloy@users.noreply.github.com>
2023-01-18 20:11:22 +0900

    up

--- Downsample/Downsample_Inscopix.m
+++ Downsample/Downsample_Inscopix.m
@@ -15,7 +15,7 @@ for k=1:length(theFiles)
     fullFileName = theFiles{k};
     fprintf(1, 'Now reading %s\n', fullFileName);
     [~,name]=fileparts(theFiles{k});
-    if ~isfile([outpath,'\',name,'.h5'])
+    if ~isfile([outpath,'\',name,'_ds.h5'])
         ISXD2h5(fullFileName,ds_f,outpath)
     else
         fprintf(1, 'File %s already exist in destination folder\n', [name,'.h5']);
