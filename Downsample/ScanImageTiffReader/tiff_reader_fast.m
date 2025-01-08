34f1666bf9caeb9a6c5e67d5329d2e701a4b3e21
Vergaloy <42758185+vergaloy@users.noreply.github.com>
2025-01-08 14:36:34 +0900

    fix tiff reading problem in mac

--- Downsample/ScanImageTiffReader/tiff_reader_fast.m
+++ Downsample/ScanImageTiffReader/tiff_reader_fast.m
@@ -1,4 +1,8 @@
 function out=tiff_reader_fast(fullFileName)
-
-out=ScanImageTiffReader(char(fullFileName));  %
-out=out.data();
+os = computer;
+if strcmp(os, 'PCWIN64') || strcmp(os, 'PCWIN32') % Check for Windows (64-bit or 32-bit)
+    out=ScanImageTiffReader(char(fullFileName));  %
+    out=out.data();
+else
+    out=parallelReadTiff(fullFileName);
+end
