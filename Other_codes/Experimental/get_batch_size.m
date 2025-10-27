cd73fb533bb4a9b8271f5a974892b0a55ca0b1b0
vergaloy_macbook <42758185+vergaloy@users.noreply.github.com>
2025-10-27 17:29:12 +0900

    Refactor batch size calculation for overlapping patches
    Updated the batch size computation to account for overlapping patches by introducing a multiplier based on patch dimensions and overlap. The logic now adjusts the chunk size accordingly and ensures it does not exceed the number of frames.

--- Other_codes/Experimental/get_batch_size.m
+++ Other_codes/Experimental/get_batch_size.m
@@ -9,19 +9,34 @@ end
 dims=neuron.P.mat_data.dims;
 gF=dims(3);
 
-[neuron.CaliAli_options.inter_session_alignment.batch_sz, ~] = compute_auto_batch_size(neuron.CaliAli_options.inter_session_alignment.batch_sz,[],[dims(1),dims(2)]);
+[chunk, ~] = compute_auto_batch_size(neuron.CaliAli_options.inter_session_alignment.batch_sz,[],[dims(1),dims(2)]);
 
-neuron.CaliAli_options.inter_session_alignment.batch_sz=neuron.CaliAli_options.inter_session_alignment.batch_sz/2;
-if neuron.CaliAli_options.inter_session_alignment.batch_sz>0
 
-    if gF<neuron.CaliAli_options.inter_session_alignment.batch_sz
+mat_data = neuron.P.mat_data;
+patch_pos = mat_data.patch_pos;
+[nr_patch, nc_patch] = size(patch_pos);
+if nr_patch*nc_patch>1
+    try
+        mult=max(CaliAli_options.cnmf.pars_envs.patch_dims)*CaliAli_options.cnmf.pars_envs.w_overlap/prod(CaliAli_options.cnmf.pars_envs.patch_dims);
+    catch
+        mult=0.5;
+    end
+else
+mult=0;
+end
+
+chunk=chunk+chunk*mult;  %adjust for overlaping patches
+
+if chunk>0
+
+    if gF<chunk
         fprintf(1, 'The defined batch size (%d) is larger than the number of frames in the video (%d). Processing the entire video in a single batch.\n', ...
-            neuron.CaliAli_options.inter_session_alignment.batch_sz,gF);
-        neuron.CaliAli_options.inter_session_alignment.batch_sz=gF;
+            chunk,gF);
+        chunk=gF;
     end
 
 
-    F=diff( round(linspace(0,gF,round(gF/neuron.CaliAli_options.inter_session_alignment.batch_sz)+1)  )  );
+    F=diff( round(linspace(0,gF,round(gF/chunk)+1)  )  );
 end
 
 %% Sanity check
